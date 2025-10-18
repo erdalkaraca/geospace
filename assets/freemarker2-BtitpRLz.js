0cb2791ea398f4b17f01d42812e6ab5adca29e96
erdalkaraca <erdalkaraca@users.noreply.github.com>
2025-10-18 14:58:04 +0000

    deploy: 0932c1917194cdf18b7e64251d72aee8f117b2a2

--- assets/freemarker2-BtitpRLz.js
+++ assets/freemarker2-BtitpRLz.js
@@ -1,4 +1,4 @@
-import{g as f}from"./main-Dy0maaU_.js";import"./index-BWCrRnvf.js";/*!-----------------------------------------------------------------------------
+import{g as f}from"./main-D_bXfQ7g.js";import"./index-BWCrRnvf.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
