4b471e2604025b7ef56112a7f4970a426ccf8f4c
erdalkaraca <erdalkaraca@users.noreply.github.com>
2025-11-20 13:09:17 +0000

    deploy: ff6a1a00fbbccc69983934884a3699f28aefa692

--- assets/in-browser-ml-extension-CkPQto1e.js
+++
@@ -1,1 +0,0 @@
-import{r,L as o}from"./geospace-app-Cg0HVUzX.js";import"./preload-helper-WQhrSmCl.js";import"./index-B0p40qid.js";const s=({})=>{r.put("inBrowserMLService",o)};export{s as default};
---
+++ assets/in-browser-ml-extension-emvGjU37.js
@@ -0,0 +1,1 @@
+import{r,L as o}from"./geospace-app-DOqKRYZp.js";import"./preload-helper-WQhrSmCl.js";import"./index-BPuo-xLZ.js";const s=({})=>{r.put("inBrowserMLService",o)};export{s as default};
