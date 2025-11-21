09875c256538431e1edcc6a2d011dc0431cbede9
Erdal Karaca <erdal.karaca.de@gmail.com>
2025-11-21 12:42:06 +0000

    Refactor: update Lit API exports and streamline WebAwesome integration

--- packages/gs-lib/src/gs-litns.ts
+++ packages/gs-lib/src/gs-litns.ts
@@ -1,8 +1,5 @@
 // Re-export all Lit APIs from appspace's externals/lit module - single source of truth
-export * from "@kispace-io/appspace/externals/lit-exports";
+export * as lit from "@kispace-io/appspace/externals/lit-exports";
 
-// Import WebAwesome components from appspace's externals/webawesome module
-// This maintains a single source of truth - Vite will optimize the build to only include what's needed
-// WebAwesome is treated as part of lit, so it's always available to user modules
 import "@kispace-io/appspace/externals/webawesome";
 
