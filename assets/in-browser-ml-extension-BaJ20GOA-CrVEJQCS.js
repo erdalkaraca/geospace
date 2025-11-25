e6d3e2c472118812af07f78838315d44b60b0e79
erdalkaraca <erdalkaraca@users.noreply.github.com>
2025-11-25 21:08:30 +0000

    deploy: ac9753237329217572b7dc21a38c414613885dd4

--- assets/in-browser-ml-extension-BaJ20GOA-CrVEJQCS.js
+++
@@ -1,1 +0,0 @@
-import{az as r,aH as o}from"./index-BgkfMran.js";import"./preload-helper-WQhrSmCl.js";import"./chunk.LTSJC6DR-BCCP1Gxs.js";const s=({})=>{r.put("inBrowserMLService",o)};export{s as default};
---
+++ assets/in-browser-ml-extension-DzlSkaxu-CcHWeCp8.js
@@ -0,0 +1,1 @@
+import{aC as r,aD as o}from"./index-CGXaaVt9.js";import"./preload-helper-WQhrSmCl.js";import"./chunk.LTSJC6DR-BCCP1Gxs.js";const s=({})=>{r.put("inBrowserMLService",o)};export{s as default};
